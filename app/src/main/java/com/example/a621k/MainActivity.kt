69e42b0de2f421ff98a1b6c504a4a56c16b42695
eldorturgunov777 <eldorturgunov777@gmail.com>
2022-06-20 18:45:18 +0500

    new

--- app/src/main/java/com/example/a621k/MainActivity.kt
+++ app/src/main/java/com/example/a621k/MainActivity.kt
@@ -1,42 +1,45 @@
 package com.example.a621k
 
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.a621k.manager.PrefsManager
 import java.lang.Boolean
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var et_string: EditText
     lateinit var et_float: EditText
-    lateinit var et_boolean: EditText
     lateinit var et_integer: EditText
+    lateinit var _switch: Switch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
+        save()
     }
 
     private fun init() {
         et_string = findViewById(R.id.et_string)
-        et_float = findViewById(R.id.et_float)
-        et_boolean = findViewById(R.id.et_boolean)
         et_integer = findViewById(R.id.et_integer)
-
+        et_float = findViewById(R.id.et_float)
+        _switch = findViewById(R.id._switch)
     }
 
-    fun save(view: View) {
-        val name = et_string.text.toString()
-        val number = et_integer.text.toString().toInt()
-        val floatD = et_float.text.toString().toFloat()
-        val booleanD = Boolean.parseBoolean(et_boolean.text.toString())
-
-        PrefsManager.getInstance(this)!!.saveStringData("name", name)
-        PrefsManager.getInstance(this)!!.saveIntData("number", number)
-        PrefsManager.getInstance(this)!!.saveFloatData("float", floatD)
-        PrefsManager.getInstance(this)!!.saveBooleanData("boolean", booleanD)
+    fun save() {
+        val save = findViewById<Button>(R.id.save)
+        save.setOnClickListener {
+            val sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putLong("long", et_string.text.toString().toLong())
+            editor.putInt("int", et_integer.text.toString().toInt())
+            editor.putFloat("float", et_float.text.toString().toFloat())
+            editor.putBoolean("boolean", _switch.isChecked)
+            editor.apply()
+            Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show()
+        }
     }
-
 }
